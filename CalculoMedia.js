74405c15e47d28ea0701b684e040fc309706507e
Danielle Gaspar <danielle.aquinno@hotmail.com>
2022-02-07 15:55:40 -0300

    Add files via upload

---
+++ CalculoMedia.js
@@ -0,0 +1,10 @@
+// Calcular a média das notas e um aluno //
+
+let trabalhoDeHistoria = 8.0
+let trabalhodeMatematica = 7.0
+let trabalhodeCiencia = 10
+let trabalhodeGeografia = 9.5
+let soma = trabalhoDeHistoria + trabalhodeMatematica + trabalhodeCiencia + trabalhodeGeografia
+    console.log(soma)
+let media = 'A média do aluno é: ' + soma / 4
+    console.log(media)

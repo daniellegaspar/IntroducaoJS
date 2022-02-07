6cf7d9c165b75668fbd9e0bc3660d18b3ac1633a
Danielle Gaspar <danielle.aquinno@hotmail.com>
2022-02-07 15:30:41 -0300

    Add files via upload

---
+++ ExerciciosCalculo.js
@@ -0,0 +1,11 @@
+var numeroA = 6
+var numeroB = 8
+var soma = 9
+var subtracao = 12
+var multiplicacao = 2
+var divisao = 6
+
+console.log(6 + 9)  //soma//
+console.log(12 * 6) //multiplicação//
+console.log(6/2) //divisao//
+console.log(12-8) //subtração//

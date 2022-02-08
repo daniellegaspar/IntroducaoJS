46ec4edec94fd1fc598b5516b3f8a5609052a38b
Danielle Gaspar <danielle.aquinno@hotmail.com>
2022-02-08 20:31:09 -0300

    Add files via upload

---
+++ VerificarIdadeUsuario.js
@@ -0,0 +1,8 @@
+var nome = "Danielle"
+var idade = 27
+
+if (idade >=18) {
+    console.log("Olá, " + nome + " Você passou no nosso teste e já pode dirigir!.")
+} else {
+    console.log("Olá, " + nome + "!")
+}

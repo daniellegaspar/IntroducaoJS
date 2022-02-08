4e347488c552b48312b9167d33f76440c9cdb1b4
Danielle Gaspar <danielle.aquinno@hotmail.com>
2022-02-08 20:37:24 -0300

    Add files via upload

---
+++ ContaBancaria.js
@@ -0,0 +1,9 @@
+// Exercício conta bancária //
+
+var saldo = -15.58
+ if (saldo >0){
+     console.log("Seu saldo está positivo! Gostaria de fazer um investimento?")
+ }
+ if (saldo <0){
+     console.log("Seu saldo está negativo! Gostaria de um empréstimo?")
+ }

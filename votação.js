d55c0e12b069305600e03096a66f44b86562cff1
Danielle Gaspar <danielle.aquinno@hotmail.com>
2022-02-08 21:00:14 -0300

    Update votação.js

--- votação.js
+++ votação.js
@@ -1,6 +1,8 @@
-let idade = 27
+let idade = 15
     if(idade >=18){
         console.log("Você é obrigado a votar!")
-    } else {
+    } else if (idade >= 16) {
         console.log("Seu voto é opcional!")
+    } else { 
+        console.log("Você ainda não tem idade para votar!")
     }

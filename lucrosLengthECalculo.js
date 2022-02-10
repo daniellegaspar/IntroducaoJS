c753402a35227119649faa89556aca5ef779a29d
Danielle Gaspar <danielle.aquinno@hotmail.com>
2022-02-09 22:39:48 -0300

    Add files via upload

---
+++ lucrosLengthECalculo.js
@@ -0,0 +1,11 @@
+//criar um loop que calcule o valor total //
+
+var listaDeLucro = [100, 30, 300, -10, 600, 10]
+var lucroTotal = 0
+
+for(var i=0; i<listaDeLucro.length; ++i){
+    lucroTotal+=listaDeLucro[i] 
+   
+}
+console.log(lucroTotal)
+
